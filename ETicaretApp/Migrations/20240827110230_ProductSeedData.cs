b40fc05f176f08af611f4ee64473ac01ad40ea3c
furkananil <furkananil960@gmail.com>
2024-08-27 14:08:35 +0300

    Migration'lar Olusturuldu

---
+++ ETicaretApp/Migrations/20240827110230_ProductSeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ETicaretApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, 17000m, "Gaming Computer" },
+                    { 2, 1749m, "Mechanical Keyboard" },
+                    { 3, 629m, "Mouse" },
+                    { 4, 5449m, "Monitor" },
+                    { 5, 2300m, "Deck" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+        }
+    }
+}
--- ETicaretApp/Models/RepositoryContext.cs
+++ ETicaretApp/Models/RepositoryContext.cs
@@ -11,5 +11,17 @@ namespace ETicaretApp.Models
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(
+                new Product(){ProductId = 1, ProductName = "Gaming Computer" , Price = 17_000},
+                new Product(){ProductId = 2, ProductName = "Mechanical Keyboard" , Price = 1_749},
+                new Product(){ProductId = 3, ProductName = "Mouse" , Price = 629},
+                new Product(){ProductId = 4, ProductName = "Monitor" , Price = 5_449},
+                new Product(){ProductId = 5, ProductName = "Deck" , Price = 2300}
+            );
+        }
+
     }
 }
