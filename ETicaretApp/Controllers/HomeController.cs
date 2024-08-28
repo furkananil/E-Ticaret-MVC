ad5e301f6cdf21d1452ea0396ec01be900462044
furkananil <furkananil960@gmail.com>
2024-08-28 19:22:23 +0300

    Dependency Injection ve Inversion of Control Mekanizmasi eklendi

--- ETicaretApp/Controllers/HomeController.cs
+++ ETicaretApp/Controllers/HomeController.cs
@@ -8,6 +8,6 @@ namespace ETicaretApp.Controllers
         {
             return View();
         }
-
+        
     }
 }
---
+++ ETicaretApp/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+using ETicaretApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETicaretApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //var context = new RepositoryContext(
+            //    new DbContextOptionsBuilder<RepositoryContext>().UseSqlite("Data Source = C:\\Users\\LENOVO\\Desktop\\E-Ticaret-MVC\\ProductDb.db").Options
+            //);
+
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+        public IActionResult Get(int id)
+        {
+            Product product = _context.Products.First(p => p.ProductId.Equals(id));
+            return View(product);
+        }
+
+    }
+}
