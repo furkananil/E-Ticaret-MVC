340df75c12e815dcdafe27a111e0ef9732c7e5f7
furkananil <furkananil960@gmail.com>
2024-09-07 19:58:10 +0300

    Area Layout olusturuldu

---
+++ ETicaretApp/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
