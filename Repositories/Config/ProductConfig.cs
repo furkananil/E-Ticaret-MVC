d7789608125619e4033f7fc9e8da64c3cd55f9fe
furkananil <furkananil960@gmail.com>
2024-09-09 20:10:57 +0300

    Product ve Category iliskisi kuruldu ve Migration'lar yeniden olusturuldu

---
+++ Repositories/Config/ProductConfig.cs
@@ -0,0 +1,26 @@
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repositories.Config
+{
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.ProductId);
+            builder.Property(p => p.ProductName).IsRequired();
+            builder.Property(p => p.Price).IsRequired();
+
+            builder.HasData(
+                new Product() { ProductId = 1, CategoryId= 2, ProductName = "Gaming Computer", Price = 17_000 },
+                new Product() { ProductId = 2, CategoryId= 2, ProductName = "Mechanical Keyboard", Price = 1_749 },
+                new Product() { ProductId = 3, CategoryId= 2, ProductName = "Mouse", Price = 629 },
+                new Product() { ProductId = 4, CategoryId= 2, ProductName = "Monitor", Price = 5_449 },
+                new Product() { ProductId = 5, CategoryId= 2, ProductName = "Deck", Price = 2300 },
+                new Product() { ProductId = 6, CategoryId= 1, ProductName = "History", Price = 85 },
+                new Product() { ProductId = 7, CategoryId= 1, ProductName = "Hamlet", Price = 75 }
+            );
+        }
+    }
+}
