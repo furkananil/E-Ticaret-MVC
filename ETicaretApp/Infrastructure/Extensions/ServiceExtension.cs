9d15c0b2b0fc406ae5d32a1d6c8675d70a9c1611
furkananil <furkananil960@gmail.com>
2024-09-19 17:18:40 +0300

    ServiceExtension dosyasi olusturuldu ve Program.cs dosyasindaki konfigurasyon ifadeleri buraya alindi

---
+++ ETicaretApp/Infrastructure/Extensions/ServiceExtension.cs
@@ -0,0 +1,50 @@
+using Entities.Models;
+using ETicaretApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+using Services;
+using Services.Contracts;
+
+namespace ETicaretApp.Infrastructure.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureDbContext(this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(options =>
+            {
+            options.UseSqlite(configuration.GetConnectionString("sqlconnection"), b => b.MigrationsAssembly("ETicaretApp"));
+            });
+        }
+
+        public static void ConfigureSession(this IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options => 
+            {
+                options.Cookie.Name = "ETicaretApp.Session";
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+            services.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
+            services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+        }
+
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<IOrderService, OrderManager>();
+        }
+    }
+}
